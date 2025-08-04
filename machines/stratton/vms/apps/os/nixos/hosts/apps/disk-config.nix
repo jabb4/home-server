e787bae0a9af3f8601dffcfc68d4efcc906aea7a
Jabba <87302129+jabb4@users.noreply.github.com>
2025-08-04 23:10:19 +0200

    Try to add back EP02

--- machines/stratton/vms/apps/os/nixos/hosts/apps/disk-config.nix
+++ machines/stratton/vms/apps/os/nixos/hosts/apps/disk-config.nix
@@ -7,6 +7,10 @@
         content = {
           type = "gpt";
           partitions = {
+            boot = {
+              size = "1M";
+              type = "EF02";
+            };
             ESP = {
               size = "512M";
               type = "EF00";
