8dae304ae468061c1972b7f0ce1b80edf9e64ef1
Jabba <87302129+jabb4@users.noreply.github.com>
2025-08-04 22:25:28 +0200

    test to try get it working

--- machines/stratton/vms/apps/os/nixos/hosts/apps/disko.nix
+++ machines/stratton/vms/apps/os/nixos/hosts/apps/disko.nix
@@ -1,26 +1,32 @@
 {
   disko.devices = {
-    disk.nixos = {
-      type = "disk";
-      device = "/dev/sda";
-      content = {
-        type = "gpt";
-        partitions = {
-          ESP = {
-            size = "512M";
-            type = "EF00";
-            content = {
-              type = "filesystem";
-              format = "vfat";
-              mountpoint = "/boot";
+    disk = {
+      nixos = {
+        type = "disk";
+        device = "/dev/sda";
+        content = {
+          type = "gpt";
+          partitions = {
+            boot = {
+              size = "1M";
+              type = "EF02"; # for grub MBR
             };
-          };
-          root = {
-            size = "100%";
-            content = {
-              type = "filesystem";
-              format = "ext4";
-              mountpoint = "/";
+            ESP = {
+              size = "512M";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+              };
+            };
+            root = {
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/";
+              };
             };
           };
         };
