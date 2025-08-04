6c3aeaa507ee21da07626d6d3152f21c34e06b01
Jabba <87302129+jabb4@users.noreply.github.com>
2025-08-04 21:57:35 +0200

    remove mountOptions

--- machines/stratton/vms/apps/os/nixos/hosts/apps/disko.nix
+++ machines/stratton/vms/apps/os/nixos/hosts/apps/disko.nix
@@ -14,7 +14,6 @@
                 type = "filesystem";
                 format = "vfat";
                 mountpoint = "/boot";
-                mountOptions = [ "umask=0077" ];
               };
             };
             root = {
