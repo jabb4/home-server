56dbcc77181164e7a015ce54d26fd5668a158c47
Jabba <87302129+jabb4@users.noreply.github.com>
2025-08-04 22:12:36 +0200

    reformat

--- machines/stratton/vms/apps/os/nixos/hosts/apps/disko.nix
+++ machines/stratton/vms/apps/os/nixos/hosts/apps/disko.nix
@@ -1,28 +1,26 @@
 {
   disko.devices = {
-    disk = {
-      nixos = {
-        device = "/dev/sda";
-        type = "disk";
-        content = {
-          type = "gpt";
-          partitions = {
-            ESP = {
-              type = "EF00";
-              size = "512M";
-              content = {
-                type = "filesystem";
-                format = "vfat";
-                mountpoint = "/boot";
-              };
+    disk.nixos = {
+      type = "disk";
+      device = "/dev/sda";
+      content = {
+        type = "gpt";
+        partitions = {
+          ESP = {
+            size = "512M";
+            type = "EF00";
+            content = {
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/boot";
             };
-            root = {
-              size = "100%";
-              content = {
-                type = "filesystem";
-                format = "ext4";
-                mountpoint = "/";
-              };
+          };
+          root = {
+            size = "100%";
+            content = {
+              type = "filesystem";
+              format = "ext4";
+              mountpoint = "/";
             };
           };
         };
