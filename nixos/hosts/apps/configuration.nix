e968d77938b385a127e0c825f4ee01a8dd1b5c7e
Jabba <87302129+jabb4@users.noreply.github.com>
2025-08-05 22:19:08 +0200

    Comment out smb

--- nixos/hosts/apps/configuration.nix
+++ nixos/hosts/apps/configuration.nix
@@ -60,15 +60,15 @@
     
 
   # Mount SMB Share for Nextcloud
-  fileSystems."/mnt/share" = {
-    device = "//<IP_OR_HOST>/path/to/share";
-    fsType = "cifs";
-    options = let
-      # this line prevents hanging on network split
-      automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+  # fileSystems."/mnt/nextcloud" = {
+  #   device = "//<IP_OR_HOST>/nextcloud";
+  #   fsType = "cifs";
+  #   options = let
+  #     # this line prevents hanging on network split
+  #     automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
 
-    in ["${automount_opts},credentials=/etc/nixos/smb-secrets,uid=1000,gid=1000"];
-  };
+  #   in ["${automount_opts},credentials=/home/nixos/.smb-credentials,uid=1000,gid=1000"];
+  # };
 
   # Enable zsh
   programs.zsh.enable = true;
