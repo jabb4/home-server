058a21d8d62fc0b8038c85d8a7ff2f3092ee056b
Jabba <87302129+jabb4@users.noreply.github.com>
2025-08-07 17:08:00 +0200

    remove blankspace

--- nixos/hosts/apps/configuration.nix
+++ nixos/hosts/apps/configuration.nix
@@ -69,7 +69,6 @@
     fsType = "cifs";
     options = let
       automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s,user,users";
-
     in ["${automount_opts},credentials=/home/nixos/.smb-credentials,uid=1000,gid=1000"];
   };
 
