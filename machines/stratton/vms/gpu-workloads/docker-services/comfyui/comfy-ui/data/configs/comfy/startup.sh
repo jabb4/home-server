f4806eaf4bdd8171c2070878612ada8ae4026fc1
Jabba <87302129+jabb4@users.noreply.github.com>
2025-08-13 23:26:18 +0200

    Removed old comfyui stuff

--- machines/stratton/vms/gpu-workloads/docker-services/comfyui/comfy-ui/data/configs/comfy/startup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ -f "/data/config/comfy/custom_nodes/ComfyUI-Manager/requirements.txt" ]; then
-  pip install -r /data/config/comfy/custom_nodes/ComfyUI-Manager/requirements.txt
-fi
-
-if [ -f "/data/config/comfy/custom_nodes/ComfyUI-GGUF/requirements.txt" ]; then
-  pip install -r /data/config/comfy/custom_nodes/ComfyUI-GGUF/requirements.txt
-fi
